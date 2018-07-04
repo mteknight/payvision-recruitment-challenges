730bda54f5dc00e8a2ce8b34ca7a9fa049561d50
Alexandre Fernandes <mteknight@gmail.com>
2018-07-05 00:19:26 +0200

    Implemented bit count and position algorithms

---
+++ Algorithms.CountingBits/IntegerAsBinaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Payvision.CodeChallenge.Algorithms.CountingBits
+{
+    public static class IntegerAsBinaryExtensions
+    {
+        private const long M1 = 0x5555555555555555; //binary: 0101...
+        private const long M2 = 0x3333333333333333; //binary: 00110011..
+        private const long M4 = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
+
+        /// <remarks>Original code from https://en.wikipedia.org/wiki/Hamming_weight</remarks>
+        internal static int GetBitCount(this long input)
+        {
+            input -= (input >> 1) & M1; //put count of each 2 bits into those 2 bits
+            input = (input & M2) + ((input >> 2) & M2); //put count of each 4 bits into those 4 bits
+            input = (input + (input >> 4)) & M4; //put count of each 8 bits into those 8 bits
+            input += input >> 8; //put count of each 16 bits into their lowest 8 bits
+            input += input >> 16; //put count of each 32 bits into their lowest 8 bits
+            input += input >> 32; //put count of each 64 bits into their lowest 8 bits
+            return (int)(input & 0x7f);
+        }
+    }
+}
--- Algorithms.CountingBits/PositiveBitCounter.cs
+++ Algorithms.CountingBits/PositiveBitCounter.cs
@@ -1,10 +1,4 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="BitCounter.cs" company="Payvision">
-//     Payvision Copyright © 2017
-// </copyright>
-// -----------------------------------------------------------------------
-
-namespace Payvision.CodeChallenge.Algorithms.CountingBits
+﻿namespace Payvision.CodeChallenge.Algorithms.CountingBits
 {
     using System;
     using System.Collections.Generic;
@@ -13,7 +7,33 @@ namespace Payvision.CodeChallenge.Algorithms.CountingBits
     {
         public IEnumerable<int> Count(int input)
         {
-            throw new NotImplementedException();
+            if (input < 0) throw new ArgumentException(nameof(input), "Parameter must be a positive integer.");
+
+            return GetCountData(input);
+        }
+
+        private static IEnumerable<int> GetCountData(long input)
+        {
+            yield return input.GetBitCount();
+
+            foreach (var position in GetSwitchedBitPositions(input))
+            {
+                yield return position;
+            }
+        }
+
+        /// <remarks>Code adapted from https://stackoverflow.com/questions/3142867/finding-bit-positions-in-an-unsigned-32-bit-integer</remarks>
+        private static IEnumerable<int> GetSwitchedBitPositions(long input)
+        {
+            var iterator = 0;
+
+            for (var i = 1; i < 256; i <<= 1, iterator++)
+            {
+                if ((input & i) > 0)
+                {
+                    yield return iterator;
+                }
+            }
         }
     }
 }
